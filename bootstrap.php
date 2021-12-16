1d57bf4e3ff9f72573b85e810a3bfa0d3272b710
Longinus <jesuscaesar@outlook.com>
2021-12-17 09:48:26 +1000

    Latest update

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 $thisSystem = file_get_contents('system.info');
 $getSource = file_get_contents('get.cfg');
 if ($getSource == 'https://github.com' || $getSource == 'https://www.github.com') {
-    $srcPubRepo = 'flossily';
+    $srcPubRepo = 'flossely';
 } else {
     $srcPubRepo = 'webfloss';
 }
