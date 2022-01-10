eb07489b868ce26c765bbb3948199a660d1ce95e
Longinus <jesuscaesar@outlook.com>
2022-01-10 13:43:10 +1000

    Latest update

--- bottom.php
+++ bottom.php
@@ -1,4 +1,4 @@
 <img style="height:100%;" class="imgLogo" src="sys.home.png" title="Start Menu" onclick="window.location.href = 'index.php';">
+<img style="height:100%;" class="imgLogo" src="sys.files.png" title="File Explorer" onclick="window.location.href = 'explorer.php';">
 <img style="height:100%;" class="imgLogo" src="sys.upd.png" title="Update System" onclick="get('i', 'from', 'explorer', 'flossely');">
-<img style="height:100%;" class="imgLogo" src="sys.setup.png" title="Get Eurohouse!" onclick="get('r', 'explorer', 'eurohouse', 'eurohouse');">
 <img style="height:100%;" class="imgLogo" src="sys.exit.png" title="Shutdown" onclick="window.location.href = '../';">
