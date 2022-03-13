8fb82b8e143d0f03de10063c12020f9bc4b24d10
Longinus <jesuscaesar@outlook.com>
2022-03-13 08:24:19 +0000

    Latest update

--- bottom.php
+++ bottom.php
@@ -1,4 +1,4 @@
-<img style="height:100%;" class="imgLogo" src="sys.start.png" title="Start Menu" onclick="window.location.href = 'index.php';">
-<img style="height:100%;" class="imgLogo" src="sys.files.png" title="File Explorer" onclick="window.location.href = 'explorer.php';">
-<img style="height:100%;" class="imgLogo" src="sys.upd.png" title="Update System" onclick="get('i', 'from', 'explorer', 'flossely');">
-<img style="height:100%;" class="imgLogo" src="sys.exit.png" title="Shutdown" onclick="window.location.href = '../';">
+<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.start.png?raw=true" title="Start Menu" onclick="window.location.href = 'index.php';">
+<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.files.png?raw=true" title="File Explorer" onclick="window.location.href = 'explorer.php';">
+<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.upd.png?raw=true" title="Update System" onclick="get('i', 'from', 'explorer', 'flossely');">
+<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.exit.png?raw=true" title="Shutdown" onclick="window.location.href = '../';">
