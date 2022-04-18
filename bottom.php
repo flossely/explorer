13bd9b00cb4e3e350a2ba0e863a16e1f8787de28
Longinus <jesuscaesar@outlook.com>
2022-04-18 10:22:22 +0000

    Add

--- bottom.php
+++ bottom.php
@@ -1,6 +1,1 @@
-<p align='center'>
-<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.start.png?raw=true" title="Start Menu" onclick="window.location.href = 'index.php';">
-<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/state/rawd.europe.png?raw=true" title="Switch to Eurohouse" onclick="get('r', '', 'explorer', 'eurohouse', '', 'eurohouse');">
-<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.upd.png?raw=true" title="Update System" onclick="get('i', '', 'from', 'explorer', '', 'flossely');">
 <span id='ct'></span>
-</p>
