19bd9cedce2a638f77d5dcb9ce78e3a06882c53b
Longinus <jesuscaesar@outlook.com>
2022-04-18 10:27:45 +0000

    Add

--- appscript.php
+++ appscript.php
@@ -1,23 +1,6 @@
-<script src="jquery.js"></script>
-<script src="base.js"></script>
-<script src="sort.js"></script>
-<script src="http://www.midijs.net/lib/midi.js"></script>
 <script>
 window.onload = function() {
     playAudio('snd<?=$colorName;?>.flac?rev=<?=time();?>');
-}
-function playAudio(name) {
-    audioPlayer.src = name;
-    audioPlayer.play();
-}
-function pauseAudio() {
-    audioPlayer.pause();
-}
-function playMIDI(id) {
-    MIDIjs.play(id);
-}
-function pauseMIDI(id) {
-    MIDIjs.pause(id);
 }
 function initConfig(color) {
     var dataString = 'color=' + color;
