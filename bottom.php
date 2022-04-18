7b9952afb1da20e43e551e92775def8734c13045
Longinus <jesuscaesar@outlook.com>
2022-04-18 10:31:05 +0000

    Add

--- bottom.php
+++ bottom.php
@@ -1,1 +1,3 @@
-<span id='ct'></span>
+<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.start.png?raw=true" title="Start Menu" onclick="window.location.href = 'index.php';">
+<img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.upd.png?raw=true" title="Update System" onclick="get('i', '', 'from', 'explorer', '', 'flossely');">
+<input type='button' class='actionButton' id='ct'>
