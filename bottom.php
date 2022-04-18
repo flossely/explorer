02678be6c5d1bc4e5dac9d63d221986da7a50804
Longinus <jesuscaesar@outlook.com>
2022-04-18 10:33:14 +0000

    Add

--- bottom.php
+++ bottom.php
@@ -1,3 +1,3 @@
 <img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.start.png?raw=true" title="Start Menu" onclick="window.location.href = 'index.php';">
 <img style="height:100%;" class="imgLogo" src="https://github.com/wholemarket/rawd/blob/system/rawd.upd.png?raw=true" title="Update System" onclick="get('i', '', 'from', 'explorer', '', 'flossely');">
-<input type='button' class='actionButton' id='ct'>
+<input type='button' class='digitalClockDiv' id='ct'>
